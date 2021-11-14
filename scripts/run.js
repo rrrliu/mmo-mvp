9b1c8effc057dc8b0da6c1f2f232d7b2f3666147
Richard Liu <richard.y.liu@berkeley.edu>
2021-11-14 03:19:39 -0800

    created some example characters

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,15 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("Game");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Luffy", "Katakuri", "Usopp"], // Names
+    [
+      "https://imgur.com/TyemgL3", // Images
+      "https://imgur.com/3512Jr5",
+      "https://imgur.com/FWbOAS6",
+    ],
+    [300, 250, 100], // HP values
+    [100, 150, 25] // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
